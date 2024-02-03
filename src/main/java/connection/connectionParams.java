f9230a1ad418abf1f389d0ce64482add8ef458d6
Antenaina2002 <hei.antenaina@gmail.com>
2024-02-03 14:45:49 +0300

    feat: added connectionParams that contains the parameters to the database connexion

---
+++ src/main/java/connection/connectionParams.java
@@ -0,0 +1,7 @@
+package connection;
+
+public class connectionParams {
+    public static final String URL = System.getenv("URL");
+    public static final String USER = System.getenv("USER");
+    public static final String PASSWORD = System.getenv("PASSWORD");
+}
