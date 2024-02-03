19243edcd099ab685ad523be438dd4895bbd8122
Antenaina2002 <hei.antenaina@gmail.com>
2024-02-03 14:45:24 +0300

    feat: added connectionParams that contains the parameters to the database connexion

--- src/main/java/com/daw/face/DawfaceApplication.java
+++ src/main/java/com/daw/face/DawfaceApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DawfaceApplication {
-
   public static void main(String[] args) {
-    SpringApplication.run(DawfaceApplication.class, args);
+    System.out.println("hello world!");
   }
 }
