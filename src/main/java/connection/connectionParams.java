9175ee2cc89331f250724fcd58d6f0adfa15097b
Antenaina2002 <hei.antenaina@gmail.com>
2024-02-03 15:55:40 +0300

    feat: modified the connectionParameters to be in applications properties

--- src/main/java/connection/connectionParams.java
+++
@@ -1,7 +0,0 @@
-package connection;
-
-public class connectionParams {
-    public static final String URL = System.getenv("URL");
-    public static final String USER = System.getenv("USER");
-    public static final String PASSWORD = System.getenv("PASSWORD");
-}
