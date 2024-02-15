1a2b010a3744b25970fb2c9ab897d2773f9f5331
Antenaina2002 <hei.antenaina@gmail.com>
2024-02-15 19:27:27 +0300

    feat: implement password hashing using bcrypt

--- src/main/java/com/daw/face/repository/conf/SecurityConf.java
+++ src/main/java/com/daw/face/repository/conf/SecurityConf.java
@@ -38,6 +38,8 @@ public class SecurityConf {
 
         return new InMemoryUserDetailsManager(admin);
     }
+
+
 }
 
 
--- src/main/java/com/daw/face/service/UserService.java
+++ src/main/java/com/daw/face/service/UserService.java
@@ -2,20 +2,27 @@ package com.daw.face.service;
 
 import com.daw.face.repository.UserRepository;
 import com.daw.face.repository.model.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-
     private final UserRepository userRepository;
-
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
     }
 
     public List<User> findAll(){
        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
     }
 }
