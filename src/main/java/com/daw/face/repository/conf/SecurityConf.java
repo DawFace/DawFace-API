f3af6bc8113b57244d949744cea857eb3c47002b
Antenaina2002 <hei.antenaina@gmail.com>
2024-02-15 22:26:36 +0300

    feat:Implement dynamic user authentication via UserRepository

--- src/main/java/com/daw/face/repository/conf/SecurityConf.java
+++ src/main/java/com/daw/face/repository/conf/SecurityConf.java
@@ -1,17 +1,15 @@
 package com.daw.face.repository.conf;
 
+import com.daw.face.repository.UserRepository;
+import com.daw.face.repository.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,16 +27,15 @@ public class SecurityConf {
         return http.build();
     }
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails admin = User.builder()
-                .username("kim")
-                .password(passwordEncoder().encode("dawson"))
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin);
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByEmail(username);
+            if (user == null) {
+                throw new UsernameNotFoundException("User not found");
+            }
+            return UserDetailsServicesImpl.create(user);
+        };
     }
-
 
 }
 
