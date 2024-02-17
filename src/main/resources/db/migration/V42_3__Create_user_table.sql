f7eab4d4ed27f3ddf6955901905112e754d81ee0
Tendry24 <hei.tendry.2@gmail.com>
2024-02-17 16:27:08 +0300

    refactor(V42_4): move user insert queries to appropriate file

--- src/main/resources/db/migration/V42_3__Create_user_table.sql
+++ src/main/resources/db/migration/V42_3__Create_user_table.sql
@@ -7,11 +7,3 @@ create table "user" (
     picture varchar(255) not null
 );
 
-INSERT INTO "user" (last_name, first_name, email, password, picture)
-VALUES ('Tendry', 'Rakoto', 'tendry@kim.com', 'uwu', 's3://dawface-image-storage/Tendry.jpg');
-
-INSERT INTO "user" (last_name, first_name, email, password, picture)
-VALUES ('Tanjona', 'Niaina', 'tanjona@kim.com', 'mia', 's3://dawface-image-storage/Tanjona.jpg');
-
-INSERT INTO "user" (last_name, first_name, email, password, picture)
-VALUES ('Ante', 'Malone', 'Ante@kim.com', 'san', 's3://dawface-image-storage/Ante.jpg');
---
+++ src/main/resources/db/migration/V42_4__Insert_into_user_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Tendry', 'Rakoto', 'tendry@kim.com', 'uwu', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Tendry.jpg');
+
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Tanjona', 'Niaina', 'tanjona@kim.com', 'mia', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Tanjona.jpg');
+
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Ante', 'Malone', 'Ante@kim.com', 'san', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Ante.jpg');
