0702e5807f857e242957faec970f2a8a0e2b9017
Tendry24 <hei.tendry.2@gmail.com>
2024-02-18 20:12:26 +0300

    feat: add new insert queries for user resources

--- src/main/resources/db/migration/V42_4__Insert_into_user_table.sql
+++ src/main/resources/db/migration/V42_4__Insert_into_user_table.sql
@@ -6,3 +6,21 @@ VALUES ('Tanjona', 'Niaina', 'tanjona@kim.com', 'mia', 'https://dawface-bucket.s
 
 INSERT INTO "user" (last_name, first_name, email, password, picture)
 VALUES ('Ante', 'Malone', 'Ante@kim.com', 'san', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Ante.jpg');
+
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Andy', 'Avotiana', 'Andy@kim.com', 'mar', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Andy.jpg');
+
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Fiantso', 'Harena', 'Fiantso@kim.com', 'eli', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Fiantso.jpg');
+
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Sheldon', 'Cooper', 'Sheldon@kim.com', 'lie', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Sheldon-Cooper.jpg');
+
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Rajesh', 'kootrapali', 'Rajesh@kim.com', 'jar', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Rajesh.jpg');
+
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Howard', 'wolowitz', 'howard@kim.com', 'woh', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Howard.jpg');
+
+INSERT INTO "user" (last_name, first_name, email, password, picture)
+VALUES ('Leonard', 'hofstader', 'leonard@kim.com', 'mmm', 'https://dawface-bucket.s3.eu-west-3.amazonaws.com/Leonard.png');
