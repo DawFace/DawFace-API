e96347a799f4d09ee617d688e33d602c0c181a1e
tanjonaaa <hei.tanjona@gmail.com>
2024-02-18 06:29:19 +0100

    chore: enabling CORS for /api

--- src/main/java/com/daw/face/endpoint/rest/controller/UserController.java
+++ src/main/java/com/daw/face/endpoint/rest/controller/UserController.java
@@ -5,9 +5,11 @@ import com.daw.face.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UserController {
